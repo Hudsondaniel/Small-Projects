edad6005d523239bfed794c992c766cf859a09c3
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-02-28 09:57:57 +0530

    - Add other small projects to the repo & commit

---
+++ Recipies/README.md
@@ -0,0 +1,7 @@
+This is a recipie website!!
+
+-- Gonna be very simple though
+
+To access this website goto
+
+https://hudsondaniel.github.io/Recipies/
---
+++ Rock-Paper-scissor/app.js
@@ -0,0 +1,4 @@
+const rock = document.getElementsByClassName("rock");
+const paper = document.getElementsByClassName("paper");
+const scissor = document.getElementsByClassName("scissor");
+
