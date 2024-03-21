6da859fabc02242163edd62ebe83534023d4fbc3
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-03-21 11:13:35 +0530

    Built object for the books and setup all event listeners

--- Library/library-script.js
+++ Library/library-script.js
@@ -1,7 +1,35 @@
-const button = document.querySelector('button');
+let newBookButton = document.querySelector('#new-book-button');
+let newBookForm = document.querySelector('#new-book-form');
+newBookForm.style.display = 'none';
 
-button.addEventListener('click', () => {
-    const name = prompt('What is your name?');
-    const greeting = document.querySelector('#greeting');
-    greeting.textContent = `Hello ${name}, nice to see you!`;
+let library = [];
+
+function addBook() {
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let numberOfPages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read-checkbox').checked;
+    let newBook = new Book(title, author, numberOfPages, read);
+    library.push(newBook);
+    console.log(library);
+    //library.push(newBook);
+};
+
+function Book(title, author, numberOfPages, read) {
+
+     this.title = title;
+     this.author = author;
+     this.numberOfPages = numberOfPages;
+     this.read = read;
+
+};
+
+document.querySelector('#submit-button').addEventListener('click', () => {
+    event.preventDefault();
+    addBook();
+});
+
+
+newBookButton.addEventListener('click', () => {
+   newBookForm.style.display = 'block';
 });
