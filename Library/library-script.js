54edfbaeb04fc8254c3e67d5c18a083f44109a52
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-03-22 10:16:31 +0530

    Add functions for creating the book

--- Library/library-script.js
+++ Library/library-script.js
@@ -1,9 +1,30 @@
+//Array to store data
+let library = [];
+
 let newBookButton = document.querySelector('#new-book-button');
 let newBookForm = document.querySelector('#new-book-form');
 newBookForm.style.display = 'none';
 
-let library = [];
+//Activate the new book button
+newBookButton.addEventListener('click', () => {
+    newBookForm.style.display = 'block';
+ });
 
+//Submit button funciton to add new book to array
+document.querySelector('#submit-button').addEventListener('click', () => {
+    event.preventDefault();
+    addBook();
+});
+
+//Instantion of the new book
+function Book(title, author, numberOfPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
+};
+
+//Object method to add new book to library
 function addBook() {
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
@@ -12,24 +33,15 @@ function addBook() {
     let newBook = new Book(title, author, numberOfPages, read);
     library.push(newBook);
     console.log(library);
-    //library.push(newBook);
 };
 
-function Book(title, author, numberOfPages, read) {
-
-     this.title = title;
-     this.author = author;
-     this.numberOfPages = numberOfPages;
-     this.read = read;
-
-};
-
-document.querySelector('#submit-button').addEventListener('click', () => {
-    event.preventDefault();
-    addBook();
-});
-
-
-newBookButton.addEventListener('click', () => {
-   newBookForm.style.display = 'block';
-});
+//Renders the book as cards in a div element
+function renderBook() {
+    let libraryElement = document.querySelector('.library');
+    for(const element of library) {
+        let book = element;
+        let newBook = document.createElement('div');
+       
+    };
+    
+}
