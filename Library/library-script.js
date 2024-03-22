fa00e51c10e40cf5299f7ae126a278388d8e2da9
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-03-22 11:35:27 +0530

    Javascript complete for backend

--- Library/library-script.js
+++ Library/library-script.js
@@ -24,6 +24,42 @@ function Book(title, author, numberOfPages, read) {
     this.read = read;
 };
 
+Book.prototype.toggleRead = function () {
+    this.read =!this.read;
+};
+
+function toggleRead(index) {
+    library[index].toggleRead();
+    renderBook();
+};
+
+//Function to delete the book   
+function removeBook(index) {
+    library.splice(index, 1)
+    renderBook();
+};
+
+//Renders the book
+function renderBook() {
+    let libraryElement = document.querySelector('.library');
+    libraryElement.innerHTML = '';
+    for(const element of library.entries()) {
+        const [index, book] = element;
+        let newBook = document.createElement('div');
+        newBook.innerHTML = `
+        <div class="card-header">
+            <h3 class = "title"> ${book.title} </h3>
+            <h5 class = "author">${book.author}</h5>
+        </div>
+        <div class="card-body">
+             <p>${book.numberOfPages} pages</p>
+             <p class = "read-status">${book.read ? "read" : "Not read yet"} </p>
+             <button class ="remove-button" onClick ="removeBook(${index})"> Remove</button>
+             <button class = "toggle-read-button" onClick ="toggleRead(${index})">Toggle Read</button>`;
+        libraryElement.appendChild(newBook);
+    };  
+}
+
 //Object method to add new book to library
 function addBook() {
     let title = document.querySelector('#title').value;
@@ -32,16 +68,8 @@ function addBook() {
     let read = document.querySelector('#read-checkbox').checked;
     let newBook = new Book(title, author, numberOfPages, read);
     library.push(newBook);
+    renderBook();
     console.log(library);
 };
 
 //Renders the book as cards in a div element
-function renderBook() {
-    let libraryElement = document.querySelector('.library');
-    for(const element of library) {
-        let book = element;
-        let newBook = document.createElement('div');
-       
-    };
-    
-}
