6f7d6dc2587d8762b2b0f0fa761cef287ac391b6
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-05-07 22:39:17 +0530

    Made a carousal area and then figured out why there was so much space in the containers by default. Because of the repeat function. Whoops

--- Restaurant-Page/src/Home/home.js
+++ Restaurant-Page/src/Home/home.js
@@ -39,8 +39,52 @@ function homePage() {
     firstContainerImage.setAttribute('src', coverImg);
     firstContainerImgDiv.appendChild(firstContainerImage);
     
-    //
+    //Adding data into the second container
+
+    const secondContainerLettersDiv = document.createElement('div');
+    secondContainerLettersDiv.classList.add('second-Container-LettersDiv');
+    secondContainer.appendChild(secondContainerLettersDiv);
+    const secondContainerLetters = document.createElement('h1');
+    const title = document.createElement('h1');
+    const subheader = document.createElement('h3');
+    title.textContent = 'KOREAN BOWL & BOA';
+    title.classList.add('second-div-title');
+    subheader.textContent = 'We cook up bright, bold, and unique korean bowls. And we serve it all hot, fast';
+    secondContainerLettersDiv.append(title);
+    secondContainerLettersDiv.appendChild(subheader);
+
+    //Creating another container within the second container for the pics carousal
+
+    const secondContainerPicDiv = document.createElement('div');
+    secondContainerPicDiv.classList.add('second-Container-PicDiv');
+    secondContainer.appendChild(secondContainerPicDiv);
+
+    //Adding 6 containers for pics carousal 
+
+    const firstPic = document.createElement('div');
+    firstPic.classList.add('carousal-Pic');
+    secondContainerPicDiv.appendChild(firstPic);
+    const img1 = document.createElement('img');
+    img1.classList.add('carousal-Img');
+    img1.setAttribute('src', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
+    firstPic.appendChild(img1);
     
+    const secondPic = document.createElement('div');
+    secondPic.classList.add('carousal-Pic');
+    secondContainerPicDiv.appendChild(secondPic);
+    const thirdPic = document.createElement('div');
+    thirdPic.classList.add('carousal-Pic');
+    secondContainerPicDiv.appendChild(thirdPic);
+    const fourthPic = document.createElement('div');
+    fourthPic.classList.add('carousal-Pic');
+    secondContainerPicDiv.appendChild(fourthPic);
+    const fifthPic = document.createElement('div');
+    fifthPic.classList.add('carousal-Pic');
+    secondContainerPicDiv.appendChild(fifthPic);
+    const sixthPic = document.createElement('div');
+    sixthPic.classList.add('carousal-Pic');
+    secondContainerPicDiv.appendChild(sixthPic);
+
 }
 
 export {homePage}
