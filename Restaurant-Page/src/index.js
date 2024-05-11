a521c70f47392bf68ff4d06112c426922eafa7cc
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-05-12 01:17:33 +0530

    Trying to implement the logic for tab switching

--- Restaurant-Page/src/index.js
+++ Restaurant-Page/src/index.js
@@ -2,10 +2,20 @@ import { homePage } from "./Home/home";
 import './style.css';
 import './home/homeStyle.css';
 
+homePage();
+
 const menuPage = document.querySelector('.menu-button');
+const homePageButton = document.querySelector('.Home-button');
+const contentDiv = document.getElementById('content');
 
 menuPage.addEventListener('click', () => {
-   
+     contentDiv.innerHTML = '';
+
 });
 
-homePage();
+homePageButton.addEventListener('click', () => {
+    const homePageContent = homePage();
+    contentDiv.innerHTML = homePageContent;
+})
+
+
