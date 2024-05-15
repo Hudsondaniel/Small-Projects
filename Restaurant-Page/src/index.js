fe1776b5af794caee150ecdd0b891b9674b3e18f
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-05-15 19:03:25 +0530

    There was some issue with index file. Replaced with a new index.js file.

--- Restaurant-Page/src/index.js
+++ Restaurant-Page/src/index.js
@@ -2,8 +2,5 @@ import { homePage } from "./Home/home";
 import './style.css';
 import './home/homeStyle.css';
 import { menuPage } from "./Menu/menu";
-import pageLoad from "./pageLoad";
-import initialLoad from "./pageLoad";
-import createTabs from "./tabs";
 
 homePage();
