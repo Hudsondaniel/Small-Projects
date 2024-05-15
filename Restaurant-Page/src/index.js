ec977f60eb416595a67282d80a17e87d98e41fc3
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-05-15 19:07:16 +0530

    Trying the tab switching logic again from this commit onwards

--- Restaurant-Page/src/index.js
+++ Restaurant-Page/src/index.js
@@ -1,6 +1,8 @@
 import { homePage } from "./Home/home";
 import './style.css';
 import './home/homeStyle.css';
+import './Menu/menuStyle.css';
+import './about/aboutStyle.css';
 import { menuPage } from "./Menu/menu";
 
 homePage();
