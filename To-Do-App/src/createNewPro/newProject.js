3767eff378ffa9187c4eb6a92d2cba90b478af02
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-05-31 15:37:43 +0530

    Made a pop up box for the new project section

--- To-Do-App/src/createNewPro/newProject.js
+++ To-Do-App/src/createNewPro/newProject.js
@@ -2,9 +2,14 @@ const plusButton = document.querySelector(".plus-symbol");
 const newProjectList = document.querySelector(".new-project");
 
 function createProject(){
-    
     plusButton.addEventListener('click', function(e){
-        
+        newProjectList.innerHTML = `
+        <div class = "overlay">
+            <div class = "pop-up-content">
+                <input type="text" name="" id="" placeholder="Add a new project">
+                <button class="continue">Continue</button>
+            </div>
+        </div>`;
     });
 }
 
